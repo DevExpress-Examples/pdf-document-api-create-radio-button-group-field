a929a0b77cbebac175040ebcd82985d956430300
DevExpressExampleBot <fakeEmail>
2025-12-25 02:43:48 +0400

    Source auto update [skip ci]

--- VB/AddRadioButtonField/Program.vb
+++ VB/AddRadioButtonField/Program.vb
@@ -17,8 +17,8 @@ Namespace AddRadioButtonField
                     processor.RenderNewPage(PdfPaperSize.Letter, graphics)
                 End Using
             End Using
-            Process.Start(New ProcessStartInfo("..\..\..\Result.pdf") With {.UseShellExecute = True})
 
+            Process.Start(New ProcessStartInfo("..\..\..\Result.pdf") With {.UseShellExecute = True})
         End Sub
 
         Private Shared Sub DrawRadioButtonGroupField(ByVal graphics As PdfGraphics)
